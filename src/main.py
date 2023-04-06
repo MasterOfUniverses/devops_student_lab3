950e1fe93289dda32f6fb533d8b97963ec27275f
MasterOfUniverses <DrStephenVincentStrange@yandex.ru>
2023-04-06 14:11:22 +0300

    new_main_flake_v2

--- src/main.py
+++ src/main.py
@@ -3,55 +3,55 @@ import sympy
 
 
 def fibonacchi_mod(pos: int) -> str:
-i = 1
-nums = [1, 1]
-while i < pos:
-	i += 1
-	nums = [nums[1], nums[0] + nums[1]]
-c = str(nums[0])
-if pos == 0:
-	c = 0
-elif pos == -1:
-	c = '#'
-return c
+    i = 1
+    nums = [1, 1]
+    while i < pos:
+        i += 1
+        nums = [nums[1], nums[0] + nums[1]]
+    c = str(nums[0])
+    if pos == 0:
+        c = 0
+    elif pos == -1:
+        c = '#'
+    return c
 
 
 def prime_mod(pos: int, force: int) -> str:
-if force == 0:
-	if pos > 1000000000:
-		return """please, enter number smaller then 1 000 000 000 
-or use force-mode /P/n (if you really want to wait so long)"""
-if pos > 0:
-	c = str(sympy.prime(pos))
-else:
-	c = '#'
-return c
+    if force == 0:
+        if pos > 1000000000:
+            return """please, enter number smaller then 1 000 000 000 
+                or use force-mode /P/n (if you really want to wait so long)"""
+    if pos > 0:
+        c = str(sympy.prime(pos))
+    else:
+        c = '#'
+    return c
 
 
 class MainHandler(RequestHandler):
-	def get(self, data: str) -> int:
-	data = str(data)
-	mode = data[0]
-	if mode == 'f':
-		pos = data[2:]
-		pos = int(pos) - 1
-		result = fibonacchi_mod(pos)
-		self.write({"num": result})
-	elif mode == 'p':
-		pos = data[2:]
-		pos = int(pos)
-		result = prime_mod(pos, 0)
-		self.write({"num": result})
-	elif mode == 'P':
-		pos = data[2:]
-		pos = int(pos)
-		result = prime_mod(pos, 1)
-		self.write({"num": result})
-	else:
-		self.write({"hello": "please, write in address bar /f/n or /p/n to get n-th fibonacchi or prime number",
-					"warning": "you can try to calculate n-th prime number with n>=1 000 000 000 but it is too long and if you really want to do it enter in address bar /P/n"
-		})
-	
+    def get(self, data: str) -> int:
+        data = str(data)
+        mode = data[0]
+        if mode == 'f':
+            pos = data[2:]
+            pos = int(pos) - 1
+            result = fibonacchi_mod(pos)
+            self.write({"num": result})
+        elif mode == 'p':
+            pos = data[2:]
+            pos = int(pos)
+            result = prime_mod(pos, 0)
+            self.write({"num": result})
+        elif mode == 'P':
+            pos = data[2:]
+            pos = int(pos)
+            result = prime_mod(pos, 1)
+            self.write({"num": result})
+        else:
+            self.write({"hello": "please, write in address bar /f/n or /p/n to get n-th fibonacchi or prime number",
+                    "warning": "you can try to calculate n-th prime number with n>=1 000 000 000 but it is too long and if you really want to do it enter in address bar /P/n"
+            })
+
 
 app = Application([(r"/([f|p|P]/[0-9]+)?", MainHandler)])
 app.listen(9999)
