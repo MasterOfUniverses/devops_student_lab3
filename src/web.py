955d1312cc4a77a14c6eb69cf992277e31441080
MasterOfUniverses <DrStephenVincentStrange@yandex.ru>
2023-04-06 14:36:44 +0300

    new_main_flake_v7

--- src/web.py
+++ src/web.py
@@ -1,5 +1,3 @@
-import sys
-sys.path.append(".")
 import main
 from tornado.ioloop import IOLoop
 IOLoop.current().start()
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,7 +1,7 @@
-from tornado.testing import AsyncHTTPTestCase, gen_test
 import sys
 sys.path.append("./src/")
 import main
+from tornado.testing import AsyncHTTPTestCase, gen_test
 import json
 
 
@@ -13,7 +13,6 @@ def response_body_parse(body: bytes) -> dict:
 class Test_App(AsyncHTTPTestCase):
     def get_app(self) -> None:
         return main.app
-
 
     def test_fibonacchi(self) -> None:
         response = self.fetch('/f/0')
@@ -40,7 +39,6 @@ class Test_App(AsyncHTTPTestCase):
         self.assertEqual(response.code, 200)
         res = str(response_body_parse(response.body)["num"])
         self.assertEqual(res, "218922995834555169026")
-    
 
     def test_prime(self) -> None:
         response = self.fetch('/p/0')
@@ -63,8 +61,7 @@ class Test_App(AsyncHTTPTestCase):
         self.assertEqual(response.code, 200)
         res = str(response_body_parse(response.body)["num"])
         self.assertEqual(res, "104729")
- 
-   
+
     def test_forse_Prime(self) -> None:
         response = self.fetch('/P/0')
         self.assertEqual(response.code, 200)
@@ -82,39 +79,45 @@ class Test_App(AsyncHTTPTestCase):
         self.assertEqual(response.code, 200)
         res = str(response_body_parse(response.body)["num"])
         self.assertEqual(res, "541")
-    
 
     def test_start(self) -> None:
         response = self.fetch('/')
         self.assertEqual(response.code, 200)
         res = response_body_parse(response.body)
-        self.assertEqual(str(res["hello"]), """"please, write in address bar /f/n
+        self.assertEqual(str(res["hello"]), 
+""""please, write in address bar /f/n
 or /p/n to get n-th fibonacchi or prime number""")
-        self.assertEqual(str(res["warning"]), """you can try to calculate n-th prime number
+        self.assertEqual(str(res["warning"]),
+"""you can try to calculate n-th prime number
 with n>=1 000 000 000 but it is too long and if you really want to do
 it enter in address bar /P/n""")
 
 
     @gen_test(timeout = 360.0)
     def test_long_prime(self) -> None:
-        response = yield self.http_client.fetch(self.get_url('/P/1000000000'), request_timeout = 360.0)
+        response = yield self.http_client.fetch(self.get_url('/P/1000000000'),
+                                                 request_timeout=360.0)
         self.assertEqual(response.code, 200)
         res = str(response_body_parse(response.body)["num"])
         self.assertEqual(res, "22801763489")
-        response = yield self.http_client.fetch(self.get_url('/P/1000000001'), request_timeout = 360.0)
+        response = yield self.http_client.fetch(self.get_url('/P/1000000001'),
+                                                 request_timeout=360.0)
         self.assertEqual(response.code, 200)
         res = str(response_body_parse(response.body)["num"])
         self.assertEqual(res, "22801763513")
-        response = yield self.http_client.fetch(self.get_url('/p/1000000000'), request_timeout = 360.0)
+        response = yield self.http_client.fetch(self.get_url('/p/1000000000'),
+                                                 request_timeout=360.0)
         self.assertEqual(response.code, 200)
         res = str(response_body_parse(response.body)["num"])
         self.assertEqual(res, "22801763489")
-        response = yield self.http_client.fetch(self.get_url('/p/1000000001'), request_timeout = 360.0)
+        response = yield self.http_client.fetch(self.get_url('/p/1000000001'),
+                                                 request_timeout=360.0)
         self.assertEqual(response.code, 200)
         res = str(response_body_parse(response.body)["num"])
         self.assertEqual(res, """please, enter number smaller then 1 000 000 000
 or use force-mode /P/n (if you really want to wait so long)""")
-        response = yield self.http_client.fetch(self.get_url('/P/4000000000'), request_timeout = 360.0)
+        response = yield self.http_client.fetch(self.get_url('/P/4000000000'),
+                                                 request_timeout=360.0)
         self.assertEqual(response.code, 200)
         res = str(response_body_parse(response.body)["num"])
         self.assertEqual(res, "97011687217")
