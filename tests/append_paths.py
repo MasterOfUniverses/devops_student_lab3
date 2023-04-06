6fc2593b0c90acf04a47f605cd4f9c19999f02a9
MasterOfUniverses <DrStephenVincentStrange@yandex.ru>
2023-04-06 23:27:03 +0300

    v2.5

--- tests/append_paths.py
+++ tests/append_paths.py
@@ -4,11 +4,5 @@ import os
 
 current_path = os.path.dirname(__file__)
 parent_path = os.path.dirname(current_path)
-src_path = parent_path + "src/"
-test_path = parent_path + "test/"
-sys.path.append(src_path)
-sys.path.append(test_path)
 sys.path.append(parent_path)
 print(parent_path)
-print(test_path)
-print(src_path)
